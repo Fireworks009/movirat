fd975a6401fc0f259315461db7d5b83351595bbb
Hadi <hadealali39@gmail.com>
2024-11-25 15:18:00 +0300

    On branch main  Changes to be committed: 	new file:   Icons/close_24dp_C920D7_FILL0_wght400_GRAD0_opsz24.svg 	new file:   Icons/menu_24dp_C920D7_FILL0_wght400_GRAD0_opsz24.svg 	modified:   index.html 	modified:   script.js 	modified:   style.css

---
+++ script.js
@@ -0,0 +1,11 @@
+function showSidebar(){
+    const sideBar = document.querySelector('.sidebar');
+    sideBar.style.display = "flex";
+    return true;
+}
+
+function hideSidebar(){
+    const sideBar = document.querySelector('.sidebar');
+    sideBar.style.display = "none";
+    return true;
+}
